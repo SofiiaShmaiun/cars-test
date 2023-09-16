e8e574ead96bda166467b6283406c1131fa80874
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-09-16 23:52:47 +0300

    create Cards

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { useFavorites } from '../../FavoriteContext';
+import styles from './Cards.module.css';
+import sprite from '../../img/sprite.svg';
+import { Button } from 'components/Button/Button';
+
+export default function Cards({
+  handleClick,
+  onData,
+  currentPage,
+  adverts,
+  filterData,
+}) {
+  const { toggleFavorite, itemStates } = useFavorites();
+
+  const getData = (inputString, n) => {
+    return inputString.split(',')[n].trim();
+  };
+
+  const filteredCars = adverts.filter(product => {
+    return (
+      !filterData.brand ||
+      (product.make.toLowerCase().includes(filterData.brand.toLowerCase()) &&
+        (!parseInt(filterData.price) ||
+          parseInt(product.rentalPrice.replace('$', '')) <=
+            parseInt(filterData.price)) &&
+        !parseInt(filterData.fromValue)) ||
+      (parseInt(filterData.fromValue) <= parseInt(product.mileage) &&
+        !parseInt(filterData.toValue)) ||
+      parseInt(product.mileage) <= parseInt(filterData.toValue)
+    );
+  });
+
+  return (
+    <ul className={styles.cardList}>
+      {filteredCars.slice(0, currentPage * 8).map(item => (
+        <li key={item.id} className={styles.card}>
+          <button
+            className={styles.favoriteButton}
+            onClick={() => toggleFavorite(item)}
+          >
+            <svg
+              width="18"
+              height="18"
+              className={` ${
+                itemStates[item.id]
+                  ? styles.activeFavoriteIcon
+                  : styles.favoriteIcon
+              }`}
+            >
+              <use xlinkHref={`${sprite}#heart`} />
+            </svg>
+          </button>
+
+          <div className={styles.imageWrapper}>
+            <img
+              src={item.img || item.photoLink}
+              alt={item.description}
+              className={styles.image}
+            />
+          </div>
+
+          <div className={styles.title}>
+            <p>
+              {item.make} <span className={styles.model}>{item.model},</span>{' '}
+              {item.year}
+            </p>
+            <span>{item.rentalPrice}</span>
+          </div>
+
+          <div className={styles.information}>
+            <ul className={styles.informationList}>
+              <li>{getData(item.address, 1)}</li>
+              <li>{getData(item.address, 2)}</li>
+              <li>{item.rentalCompany}</li>
+            </ul>
+            <ul className={styles.informationList}>
+              <li>{item.type}</li>
+              <li>{item.model}</li>
+              <li>{item.mileage}</li>
+              <li
+                style={{
+                  width: '150px',
+                  overflow: 'hidden',
+                  whiteSpace: 'nowrap',
+                  textOverflow: 'ellipsis',
+                }}
+              >
+                {item.functionalities[0]}
+              </li>
+            </ul>
+          </div>
+
+          <Button
+            additionalStyles={styles.learnMoreButton}
+            onClick={() => {
+              handleClick();
+              onData(item);
+            }}
+          >
+            Learn more
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/CardsList/CardsList.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import styles from './CardsList.module.css';
+import Cards from 'components/Cards/Cards';
+import Modal from '../../components/Modal/Modal.jsx';
+import { Button } from 'components/Button/Button';
+import { Filter } from 'components/Filter/Filter';
+
+export default function CardsList({ adverts }) {
+  const [isOpenModal, setIsOpenModal] = useState();
+  const [modalData, setModalData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loadMoreButton, setLoadMoreButton] = useState(true);
+  const [filterData, setFilterData] = useState({
+    price: '',
+    brand: '',
+    fromValue: '',
+    toValue: '',
+  });
+
+  const handleOpenModal = () => {
+    setIsOpenModal(!isOpenModal);
+  };
+
+  const handleCardsData = (item, showLoadMoreButton) => {
+    setModalData(item);
+
+    if (showLoadMoreButton) {
+      setLoadMoreButton(true);
+    }
+  };
+
+  const loadMore = () => {
+    setCurrentPage(currentPage + 1);
+
+    if (currentPage * 8 >= 50) {
+      setLoadMoreButton(false);
+    }
+  };
+
+  return (
+    <section className={styles.container}>
+      <Filter filterData={filterData} setFilterData={setFilterData} />
+      <Cards
+        handleClick={() => handleOpenModal()}
+        onData={handleCardsData}
+        currentPage={currentPage}
+        adverts={adverts}
+        filterData={filterData}
+      />
+      {isOpenModal && <Modal openModal={setIsOpenModal} car={modalData} />}
+      {loadMoreButton && (
+        <button onClick={loadMore} className={styles.loadMoreButton}>
+          Load more
+        </button>
+      )}
+    </section>
+  );
+}
