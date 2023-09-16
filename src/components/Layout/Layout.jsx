7433b88b461bb274dd76c2894415ea4332b10e1a
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-09-16 23:53:38 +0300

    create navigation and layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Navigation } from '../Navigation/Navigation';
+import styles from './Layout.module.css';
+
+export const Layout = () => {
+  return (
+    <div>
+      <header className={styles.header}>
+        <Navigation />
+      </header>
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Navigation.module.css';
+
+export const Navigation = () => {
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? styles.active : styles.link)}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/catalog"
+        className={({ isActive }) => (isActive ? styles.active : styles.link)}
+      >
+        Catalog
+      </NavLink>
+      <NavLink
+        to="/favorites"
+        className={({ isActive }) => (isActive ? styles.active : styles.link)}
+      >
+        Favorites
+      </NavLink>
+    </nav>
+  );
+};
