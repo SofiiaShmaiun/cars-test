fc8b4faa42c22dfbe0aa56b5fa0ac4f4a7433958
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-09-16 23:52:33 +0300

    create main part

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+import { FavoritesProvider } from '../FavoriteContext';
+
+const Home = lazy(() => import('../pages/Home/Home'));
+const Catalog = lazy(() => import('../pages/Catalog/Catalog'));
+const Favorites = lazy(() => import('../pages/Favorites/Favorites'));
+
+function App() {
+  return (
+    <FavoritesProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+    </FavoritesProvider>
+  );
+}
+
+export default App;
